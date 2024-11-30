f31b8ae7d06fad06d558409504f5cfdb5c2b0eff
unknown <trunghieu8533@gmail.com>
2024-11-30 13:56:20 +0700

    add google OAuth & follow features

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,8 +3,11 @@ import prisma from "../db/prisma.js";
 import bcryptjs from "bcryptjs";
 import generateToken from "../utils/generateToken.js";
 
+import { OAuth2Client } from "google-auth-library";
+
+import axios from "axios";
+
 export const signup = async (req: Request, res: Response) => {
-  console.log(123);
   try {
     const { fullname, username, email, password, confirmPassword, gender } =
       req.body;
@@ -88,6 +91,111 @@ export const login = async (req: Request, res: Response) => {
     });
   } catch (error: any) {
     console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+export const loginOAuthCallback = async (req: Request, res: Response) => {
+  const { code } = req.query;
+  try {
+    const oAuth2Client = new OAuth2Client(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET,
+      process.env.GOOGLE_REDIRECT_URI
+    );
+    const r = await oAuth2Client.getToken(code as string);
+    // Make sure to set the credentials on the OAuth2 client.
+    oAuth2Client.setCredentials(r.tokens);
+    const { data: profile } = await axios.get(
+      "https://www.googleapis.com/oauth2/v1/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${oAuth2Client.credentials.access_token}`,
+        },
+      }
+    );
+    let user = await prisma.user.findUnique({ where: { id: profile.id } });
+    if (!user) {
+      const salt = await bcryptjs.genSalt(10);
+      const hashedPassword = await bcryptjs.hash("google_" + profile.id, salt);
+
+      user = await prisma.user.create({
+        data: {
+          id: profile.id,
+          fullname: profile.name,
+          username: profile.email,
+          password: hashedPassword,
+          email: profile.email,
+          bio: "",
+          loginType: "google",
+          profilePicture: profile.picture,
+        },
+      });
+    }
+    generateToken(user.id, res);
+
+    res.status(200).json({
+      id: user.id,
+      fullname: user.fullname,
+      username: user.username,
+      email: user.email,
+      bio: user.bio,
+      profilePicture: user.profilePicture,
+    });
+  } catch (error: any) {
+    console.log("Error in loginOAuthCallback controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const loginOAuth = async (req: Request, res: Response) => {
+  try {
+    const oAuth2Client = new OAuth2Client(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET,
+      process.env.GOOGLE_REDIRECT_URI
+    );
+    const scopes = [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ];
+    const authorizationUrl: string = oAuth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: scopes,
+    });
+    res.redirect(authorizationUrl);
+  } catch (error: any) {
+    console.log("Error in loginOAuth controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const toggleFollow = async (req: Request, res: Response) => {
+  try {
+    const { followerId } = req.body;
+    const follower = await prisma.user.findUnique({
+      where: { id: followerId },
+    });
+    if (!follower) {
+      return res.status(400).json({ error: "Invalid follower" });
+    }
+    const connection = await prisma.connection.findFirst({
+      where: { followerId: followerId, followingId: req.user.id },
+    });
+    if (!connection) {
+      const newConnection = await prisma.connection.create({
+        data: {
+          followerId,
+          followingId: req.user.id,
+        },
+      });
+      return res.status(200).json({newConnection});
+    } 
+    const deletedConnection = await prisma.connection.delete({
+      where: { id: connection.id },
+    });
+    return res.status(200).json({deletedConnection});
+  } catch (error: any) {
+    console.log("Error in getUserInfo controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import protectRoute from "../middlewares/protectRoute.js";
 import {
+  toggleFollow,
   getUserInfo,
   login,
+  loginOAuth,
+  loginOAuthCallback,
   logout,
   signup,
 } from "../controllers/auth.controller.js";
@@ -10,7 +13,11 @@ import {
 const router = express.Router();
 
 router.get("/getUserInfo", protectRoute, getUserInfo);
+router.post("/toggleFollow",protectRoute, toggleFollow);
 router.post("/signup", signup);
+router.get("/login/google/callback", loginOAuthCallback);
+router.get("/login/google", loginOAuth);
+
 router.post("/login", login);
 router.post("/logout", logout);
 
